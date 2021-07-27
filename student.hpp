2c22f325e55a41bf1cc193fee6c8d0c23d4e3f05
lecofarias <86627521+lecofarias@users.noreply.github.com>
2021-07-27 20:16:34 -0300

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -13,11 +13,13 @@ int mul(int a, int b);
 
 int add(int a, int b)
 {
+   a + b;
    return 0; // TODO: fix
 }
 
 // Exercise 2: implements function 'mul', that multiplies two numbers
 int mul(int a, int b)
 {
+   a * b;
    return 0; // TODO: fix
 }
